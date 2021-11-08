18527c413f8a15e2d82fd6927f49450d74e393a9
Andreas R <82566176+Andemanden@users.noreply.github.com>
2021-11-08 10:22:27 +0100

    Create main.js

---
+++ main/libraries/main.js
@@ -0,0 +1,14 @@
+function setup() {
+  amongus.sus = true;
+  var Engine=Matter.Engine,
+      Render=Matter.Render,
+      World=Matter.World,
+      Bodies=Matter.Bodies;
+      
+   
+}
+//hello2
+
+function draw() {
+//hello
+}
