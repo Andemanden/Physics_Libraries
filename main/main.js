a019bde4c498911dc2bc1f43bb890787cd7adb8b
felix-berg <felixbpedersen@gmail.com>
2021-11-12 10:20:45 +0100

    fix index.html

--- main/main.js
+++ main/main.js
@@ -9,6 +9,8 @@ var engine, world, bodies;
 
 var camera;
 
+var testpiston, testball;
+
 function setup() {
     createCanvas(800, 400);
     camera = new Camera(width / 2, height/2, width, height);
@@ -16,9 +18,16 @@ function setup() {
     engine = Engine.create();
     world  = engine.world;
 
+    testball = new Ball(width/2, height - 200, 30);
+    testpiston = new Piston(width/2, height - 50, 100, 100)
+
+    Events.on(engine, "beforeUpdate", moves);
+
     Runner.run(engine);
 }
 
 function draw() {
     background(0);
+
+    testball.show(); testpiston.show();
 }
