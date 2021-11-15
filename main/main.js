b6dabb6117d4c8496efad1df8f90317060294e8f
felix-berg <felixbpedersen@gmail.com>
2021-11-15 08:28:44 +0100

    cleaned up main

--- main/main.js
+++ main/main.js
@@ -9,17 +9,12 @@ var engine, world, bodies;
 
 var camera;
 
-var testpiston, testball;
-
 function setup() {
     createCanvas(800, 400);
     camera = new Camera(width / 2, height/2, width, height);
 
     engine = Engine.create();
     world  = engine.world;
-
-    testball = new Ball(width/2, height - 200, 30);
-    testpiston = new Piston(width/2, height - 50, 100, 100)
 
     Events.on(engine, "beforeUpdate", moves);
 
@@ -28,6 +23,4 @@ function setup() {
 
 function draw() {
     background(0);
-
-    testball.show(); testpiston.show();
 }
