042414bc3f7d1fb6faf988f6f73ec952513d9fe2
Andreas Rehné <ratiobellum@gmail.com>
2021-11-26 17:54:21 +0100

    Added commented out features

--- main/main.js
+++ main/main.js
@@ -13,7 +13,9 @@ function setup() {
     createCanvas(800, 400);
   
     camera = new Camera(width / 2, height/2, width, height);
-
+    //testball = new Ball(width/2, height - 200, 30);
+    //testpiston = new Piston(width/2, height - 50, 100, 100);
+    //rotator = new Rotator(width/2+40, height/2, 10, 200,0.2);
     engine = Engine.create();
     world  = engine.world;
 
