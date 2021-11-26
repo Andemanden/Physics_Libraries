2b92f3ff03ba255008011e1e93f67f98428508e2
Andreas Rehné <ratiobellum@gmail.com>
2021-11-26 17:41:03 +0100

    Rotator you can't beat meeehh
    The intend was mouseconstraint. But it could not find the html element. So I just wacked something together

---
+++ main/mechanisms/rotator.js
@@ -0,0 +1,22 @@
+//This code goes so hard! Feel free to screenshot!
+class Rotator {
+    /*"YOU CAN'T BEAT MEEEHH"*/
+    constructor(_x,_y,_w,_h,_rv) {
+      this.x = _x;this.y = _y; this.w = _w; this.h = _h;this.rot=_rv;
+      this.arm = new Rectangle(_x, _y, _w * 0.9, _h * 0.9,{isStatic: true});
+      this.stopplate.setcategory(ROTATOR_STOPPLATE_COLLISION);
+      Matter.Body.setAngle(this.arm.body, -1);
+      this.arm.setcategory(DEFAULT_COLLISION);
+      //The intend was mouseconstraint. But it could not find the html element
+    }
+    
+    activate() {
+      /*"And rotator runs free-wheeling"*/
+      Matter.Body.rotate(this.arm.body, this.rot);
+    }
+    
+    show() {
+      this.arm.show();
+    }
+  }
+  
